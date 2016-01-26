31ffd3890bd9ad6332e3f9a0577cc7aeac7feef9
Björn Bartels <bartels@dragon-projects.net>
2016-01-26 13:54:24 +0100

    update the files' info header

--- config.php
+++ config.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * BB's Zend Framework 2 Components
+ * 
+ * Theme 'AdminLTE'
+ *   - theme created by/taken from [Almsaeed Studio](https://almsaeedstudio.com)
+ *   
+ * @package		[MyApplication]
+ * @package		BB's Zend Framework 2 Components
+ * @package		Theme 'AdminLTE'
+ * @author		Björn Bartels [dragon-projects.net] <info@dragon-projects.net>
+ * @link		http://gitlab.dragon-projects.de:81/groups/themes
+ * @license		http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @copyright	copyright (c) 2016 Björn Bartels [dragon-projects.net] <info@dragon-projects.net>
+ */
+
 return array(
     'template_path_stack' => array(
         'default' => __DIR__ . '/view',
